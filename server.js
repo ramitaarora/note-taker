4b269cd0d91c5f2bf2ddf5c225e73fceacf1cae8
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-10-10 14:42:52 -0700

    edits

--- server.js
+++ server.js
@@ -13,15 +13,15 @@ app.use('/api', api);
 // GET Routes
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'public/index.html'))
+  res.sendFile(path.join(__dirname, './public/index.html'))
 );
 
 // Port
