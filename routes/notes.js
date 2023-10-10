7f7d6cdcf5379892e338937f8103189d2e2dc527
Ramita <104021517+ramitaarora@users.noreply.github.com>
2023-10-09 20:40:50 -0700

    adding notes to db file

--- routes/notes.js
+++ routes/notes.js
@@ -1,8 +1,28 @@
 const notes = require('express').Router();
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+const { v4: uuidv4 } = require('uuid');
 
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 });
+
+notes.post('/', (req, res) => {
+    console.log(req.body);
+    const {title, text} = req.body;
+
+    if (title && text) {
+        const data = {
+            title,
+            text,
+            id: uuidv4(),
+        }
+
+        readAndAppend(data, './db/db.json');
+        res.json('Note added')
+
+    } else {
+        res.json('Error adding note')
+    }
+})
 
 module.exports = notes;
